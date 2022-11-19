d364803e72c55fa116f2749c5eb153fa0148cfa2
aaadhilll <aratthennala@gmail.com>
2022-11-19 23:26:20 +0530

    [POST,GET,PUT,DELET]ANOTHER WAY OF DOING]

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,9 +1,16 @@
 
 
 from django.urls import path
-from book_api.views import book_list, book_create, book
+#from book_api.views import book_list, book_create, book 
+from book_api.views import BookList, BookCreate, BookDetail
 urlpatterns = [
-    path('', book_create),
-    path('list/', book_list),
-    path('<int:pk>', book)
+ #   path('', book_create),
+ #   path('list/', book_list),
+  #  path('<int:pk>', book)
+  path('list/', BookList.as_view()),
+  path('', BookCreate.as_view()),
+  path('<int:pk>', BookDetail.as_view())
 ]
+
+
+#changing of url patterns
